d4020416c9dbd62aecf11731610ba83b81be912e
Jean Vitor Vieira <jeanvitorv0@unesc.net>
2024-03-06 09:39:44 -0300

    portifolio

---
+++ script.js
@@ -0,0 +1,10 @@
+// Seleciona as tags <a> para aplicar a rolagem smooth
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
